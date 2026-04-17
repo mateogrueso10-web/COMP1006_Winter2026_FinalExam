d480838f8b9daf19caf2ac3c5c23f535c87d88c1
mateogrueso10-web <mateogrueso10@gmail.com>
2026-04-17 13:10:08 -0400

    text commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <div class="container">
+
+    <!-- display upload form -->
+    <h1>Upload a file</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <input type="file" name="fileToUpload" id="fileToUpload" class="form-control">
+        </div>
+        <input type="submit" value="Upload File" name="submit" class="btn btn-primary">
